a9c388665b88526a300b43820061fa49a4e9d5cf
apexmarketingautomations <apexmarketingautomations@gmail.com>
2026-09-10 10:05:04 -0400

    Link admin client operations center

--- app/admin/clients/[creatorId]/page.tsx
+++ app/admin/clients/[creatorId]/page.tsx
@@ -1,5 +1,5 @@
 import { Workspace } from "@/src/server/backoffice/workspace";
 export default async function ClientAdmin({params}:{params:Promise<{creatorId:string}>}) {
   const {creatorId}=await params;
-  return <><div className="office-support-launch"><a className="office-button" href={`/admin/clients/${creatorId}/support`}>Start audited support session</a></div><Workspace creatorId={creatorId} admin/></>;
+  return <><div className="office-support-launch"><a className="office-button" href={`/admin/clients/${creatorId}/support`}>Start audited support session</a><a className="office-button quiet" href={`/admin/clients/${creatorId}/operations`}>Open operations</a></div><Workspace creatorId={creatorId} admin/></>;
 }
